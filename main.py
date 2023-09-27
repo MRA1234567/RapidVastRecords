834c4821ea206fba7459dbf26aaa9f93861c560f
MRA1234567 <146185448+MRA1234567@users.noreply.github.com>
2023-09-27 11:03:54 +0000

    Rock Paper Scissors

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-print("hello world")
-print("hi")
+import random
+
+Pchoice = input("Rock, Paper or Scissors: ")
+Achoice = random.randint(1, 3)
+
+if Pchoice == "Rock":
+  if Achoice == 1:
+    print("Draw")
+  elif Achoice == 2:
+    print("You lose")
+  else:
+    print("You win")
+
+if Pchoice == "Paper":
+  if Achoice == 1:
+    print("You win")
+  elif Achoice == 2:
+    print("Draw")
+  else:
+    print("You lose")
+
+if Pchoice == "Scissors":
+  if Achoice == 1:
+    print("You lose")
+  elif Achoice == 2:
+    print("You win")
+  else:
+    print("Draw")
